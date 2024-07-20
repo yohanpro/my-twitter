78abde9e58c3a9af2dc52dd762133ad319971cf4
yohan kim <yohankim@yohanui-MacBookPro-2.local>
2024-07-20 14:19:12 +0900

    feat: Postform

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,5 @@
+
+import PostForm from './_component/PostForm';
 import Tab from './_component/Tab';
 import TabProvider from './_component/TabProvider';
 import style from './home.module.css';
@@ -6,7 +8,8 @@ export default function Home() {
   return (
     <main className={style.main}>
       <TabProvider>
-        <Tab/>
+        <Tab />
+        <PostForm/>
         <div>
           이것은 children
         </div>
